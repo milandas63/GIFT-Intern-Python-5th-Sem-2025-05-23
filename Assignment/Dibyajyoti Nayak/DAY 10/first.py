37782551a69ab11831fda988128eac493403a5e1
dibyajyotinayak92 <babalukumarnayak121212@gmail.com>
2025-06-06 12:29:46 +0530

    Add files via upload

---
+++ Assignment/Dibyajyoti Nayak/DAY 10/first.py
@@ -0,0 +1,12 @@
+def hollow_inverted_pyramid(n):
+    for i in range(n):
+        # print leading spaces
+        print("  " * i,end="")
+        
+        #print stars and spaces
+        for j in range(2  *(n-i )-1);
+        if j==0 or j==2 *(n-i)-2):
+            print("*",end="")
+        else:
+            print( " ",end="):
+                  print()
