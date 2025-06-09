12b236c76771eb3ec5288d614ee81f2b0f39ae83
KasturiSamantaray02 <kasturisamantaray604@gmail.com>
2025-06-09 10:14:29 +0530

    Update string_ascii.py

--- Assignment/Kasturi Samantaray/Day-10/string_ascii.py
+++ Assignment/Kasturi Samantaray/Day-10/string_ascii.py
@@ -1,10 +1,8 @@
-# def ascii_sum(s):
-#     sum = 0
-#     for i in s:
-#         sum = sum + ord(i)
-#     return sum
-# text = input("Enter a string : ")
-# result = ascii_sum(text)
-# print(f"The ASCII sum of the characters in the string is : {result}")
-for i in range(5):
-    print(i)
+def ascii_sum(s):
+    sum = 0
+    for i in s:
+        sum = sum + ord(i)
+    return sum
+text = input("Enter a string : ")
+result = ascii_sum(text)
+print(f"The ASCII sum of the characters in the string is : {result}")
